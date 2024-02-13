347e41acbd1d8fb15841d70f0c689275c124704a
samuelAbergel <72765079+samuelAbergel@users.noreply.github.com>
2024-02-13 14:37:56 +0200

    add userPermission and flow permission clean

--- profile_cleaner.py
+++ profile_cleaner.py
@@ -12,6 +12,7 @@ def profiles_cleaner(profile_file):
   ns = namespace(root)
   ET.register_namespace('', ns[1:-1])
   packages = ('AAKCS', 'Field_Trip', 'ELEE', 'archanaanndev', 'bcmanhelp', 'fsLtng', 'sortablegrid', 'due', 'fluent_commerce', 'alcmeon', 'Alcmeon', 'Twilio', 'TwilioSF', 'OdigoSms', 'Odigo', 'orgcheck', 'analyticsengine', 'Profile2PermSet', 'Jigsaw', 'DuplicateRecordItem', 'DuplicateRecordSet', 'LiveAgentSession', 'LiveChatTranscript', 'LiveChatVisitor', 'Location', 'MessagingEndUser', 'MessagingSession', 'ServiceResource', 'SocialPersona', 'SocialPost', 'p0pFD', 'FlowOrchestrationInstance', 'FlowOrchestrationWorkIte', 'MessagingChannel', 'SessionHijackingEventStore', 'Field_Trip', 'DidUEnjo')
+  users_permission_HS = ('CanAccessCE', 'ChatterFileLink', 'DistributeFromPersWksp', 'SendExternalEmailAvailable', 'ViewTrustMeasures', 'ViewDataLeakageEvents', 'AIViewInsightObjects', 'ApproveContract', 'AssignUserToSkill', 'ViewPlatformEvents', 'Packaging2PromoteVersion', 'AccessContentBuilder', 'ManageExchangeConfig')
 
   removedObjects = []
   removedRecordTypes = []
@@ -54,20 +55,30 @@ def profiles_cleaner(profile_file):
     if (enabled.text == 'false'):
       root.remove(externalDataSource)
 
-  fieldsMetadata = find_metadata_path_folder(common_path, sfoa_path, 'fields')
+  for userPermission in root.findall(ns + 'userPermissions'):
+    enabled = userPermission.find(ns + 'enabled')
+    userPermissionNameText = userPermission.find(ns + 'name')
+    if (enabled.text == 'false') or (userPermissionNameText is not None and any(user_permission_HS in userPermissionNameText.text for user_permission_HS in users_permission_HS)):
+      root.remove(userPermission)
+
+  fieldsMetadata = find_metadata_path_folder(common_path, sfoa_path, 'objects')
   for field in root.findall(ns + 'fieldPermissions'):
     readable = field.find(ns + 'readable')
     fieldText = field.find(ns + 'field')
     fieldTextAfterDot = fieldText.text.split('.', 1)[1] if '.' in fieldText.text else None
     if (readable.text == 'false') or (fieldText is not None and any(package in fieldText.text for package in packages)):
       root.remove(field)
-    elif not existing_metadata_in_repository(fieldsMetadata, fieldTextAfterDot, 'field'):
+    elif not existing_metadata_in_repository(fieldsMetadata, fieldText.text, 'field'):
         root.remove(field)
 
+  flowMetadata = find_metadata_path_folder(common_path, sfoa_path, 'flow')
   for flow in root.findall(ns + 'flowAccesses'):
     enabled = flow.find(ns + 'enabled')
-    if (enabled.text == 'false'):
-      root.remove(flow)
+    flowText = field.find(ns + 'flow')
+    if (enabled.text == 'false') or (flowText is not None and any(package in flowText.text for package in packages)):
+        root.remove(flow)
+    elif not existing_metadata_in_repository(flowMetadata, flowText.text, 'flow'):
+        root.remove(flow)
 
   objectMetadata = find_metadata_path_folder(common_path, sfoa_path, 'objects')
   for object in root.findall(ns + 'objectPermissions'):
@@ -139,6 +150,19 @@ def find_metadata_path_folder(common_path, sfoa_path, metadata_name):
 def existing_metadata_in_repository(possible_paths, metadata_name, end_file_path):
     if end_file_path == 'object':
         return any(os.path.exists(path + '\\' + metadata_name + '\\' + metadata_name + '.' + end_file_path +  '-meta.xml') for path in possible_paths)
+    if end_file_path == 'field':
+        obj = metadata_name.split('.', 1)[0] if '.' in metadata_name else None
+        fieldText =  metadata_name.split('.', 1)[1] if '.' in metadata_name else None
+        if 'Task' in obj:
+            return True
+            # return any(os.path.exists(path + '\\' + obj + '\\fields\\' + fieldText.replace('Task', 'Activity') + '.' + end_file_path +  '-meta.xml') for path in possible_paths) or any(os.path.exists(path + '\\' + obj + '\\fields\\' + fieldText + '.' + end_file_path +  '-meta.xml') for path in possible_paths)
+        if 'Event' in obj:
+            return True
+        if 'Activity' in obj:
+            return True
+            # return any(os.path.exists(path + '\\' + obj + '\\fields\\' + fieldText.replace('Event', 'Activity') + '.' + end_file_path +  '-meta.xml') for path in possible_paths) or any(os.path.exists(path + '\\' + obj + '\\fields\\' + fieldText + '.' + end_file_path +  '-meta.xml') for path in possible_paths)
+        else:
+            return any(os.path.exists(path + '\\' + obj + '\\fields\\' + fieldText + '.' + end_file_path +  '-meta.xml') for path in possible_paths)
     return any(os.path.exists(path + '\\' + metadata_name + '.' + end_file_path +  '-meta.xml') for path in possible_paths)
 
 if __name__ == '__main__':
@@ -149,4 +173,4 @@ if __name__ == '__main__':
     for file in files:
       if file.endswith('.profile-meta.xml'): 
         profile_file_path = os.path.join(root, file)
-        profiles_cleaner(profile_file_path) 
+        profiles_cleaner(profile_file_path)
